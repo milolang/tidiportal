eca8738d00c08cbd684c9be957fae0c4ef76199c
milolang <milolang@gmail.com>
2015-05-14 14:23:30 -0400

    base.read.js testing

--- service/table/Base.read.js
+++ service/table/Base.read.js
@@ -1,5 +1,17 @@
-function read(query, user, request) {
-    console.log(user + " " + request);
-    request.execute();    
+//function read(query, user, request) {
 
+
+//    request.execute();    
+
+//}
+function read(query, user, request) {
+    request.execute({
+        success: function (results) {
+            results.forEach(function (r) {
+                r.scriptComment =
+                'this was added by a script after querying the database';
+            });
+            request.respond();
+        }
+    });
 }
